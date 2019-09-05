69270dca7a950e53ee6e364885811f25d8f234e9
oninbo <oninbo@gmail.com>
2019-09-05 23:58:31 +0300

    added furniture setting to new class

--- Source/RoomGenerator/FurnitureSetter.cpp
+++ Source/RoomGenerator/FurnitureSetter.cpp
@@ -2,34 +2,34 @@
 
 
 #include "FurnitureSetter.h"
-#include "Engine\LevelStreamingDynamic.h"
+#include "Engine/LevelStreamingDynamic.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AFurnitureSetter::AFurnitureSetter()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	ULevelStreamingDynamic* StreamingLevel = NewObject<ULevelStreamingDynamic>((UObject*)GetTransientPackage(), ULevelStreamingDynamic::StaticClass());
 
-	// Add delegates
-	StreamingLevel->OnLevelLoaded.AddDynamic(this, &AFurnitureSetter::OnLevelLoaded);
-
-	StreamingLevel->bShouldBlockOnLoad = false;
-	StreamingLevel->bInitiallyLoaded = true;
-	StreamingLevel->bInitiallyVisible = true;
-
-	GetWorld()->AddStreamingLevel(StreamingLevel);
+	BedsCounter = FMath::RandRange(BedsMin, BedsMax);
+	WallSidedCounter = FMath::RandRange(WallSidedMin, WallSidedMax);
+	TablesCounter = FMath::RandRange(TablesMin, TablesMax);
+	ChairsCounter = FMath::RandRange(ChairsMin, ChairsMax);
 }
 
 // Called when the game starts or when spawned
 void AFurnitureSetter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	GetFurniture();
 }
 
-void AFurnitureSetter::OnLevelLoaded()
+void AFurnitureSetter::GetFurniture()
 {
+	UGameplayStatics::GetAllActorsWithTag(this, TEXT("Bed"), Beds);
+	UGameplayStatics::GetAllActorsWithTag(this, TEXT("WallSide"), WallSided); // TODO: remove beds from wallside
+	UGameplayStatics::GetAllActorsWithTag(this, TEXT("Table"), Tables);
+	UGameplayStatics::GetAllActorsWithTag(this, TEXT("Chair"), Chairs);
 }
 
 // Called every frame
@@ -44,5 +44,36 @@ void AFurnitureSetter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+AActor* AFurnitureSetter::TakeFurniture()
+{
+	AActor* Furniture = nullptr;
+	if (Beds.Num() && BedsCounter)
+	{
+		Furniture = Beds[FMath::RandRange(0, Beds.Num()-1)];
+		--BedsCounter;
+	}
+	else if (WallSided.Num() && WallSidedCounter)
+	{
+		Furniture = WallSided[FMath::RandRange(0, WallSided.Num()-1)];
+		--WallSidedCounter;
+	}
+	else if (Tables.Num() && TablesCounter)
+	{
+		Furniture = Tables[FMath::RandRange(0, Tables.Num()-1)];
+		--TablesCounter;
+	}
+	else if (Chairs.Num() && ChairsCounter)
+	{
+		Furniture = Chairs[FMath::RandRange(0, Chairs.Num()-1)];
+		--ChairsCounter;
+	}
+	return Furniture;
+}
+
+void AFurnitureSetter::MoveFurniture(AActor* Furniture, FVector Location)
+{
+	Furniture->SetActorLocation(Location);
 }
 
--- Source/RoomGenerator/FurnitureSetter.h
+++ Source/RoomGenerator/FurnitureSetter.h
@@ -19,7 +19,18 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	virtual void OnLevelLoaded();
+	UFUNCTION()
+	virtual void GetFurniture();
+
+	TArray<AActor*> Beds;
+	TArray<AActor*> WallSided;
+	TArray<AActor*> Tables;
+	TArray<AActor*> Chairs;
+
+	int32 BedsCounter;
+	int32 WallSidedCounter;
+	int32 TablesCounter;
+	int32 ChairsCounter;
 
 public:	
 	// Called every frame
@@ -28,4 +39,26 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditDefaultsOnly)
+	int32 BedsMin = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 BedsMax = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 WallSidedMin = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 WallSidedMax = 3;
+	UPROPERTY(EditDefaultsOnly)
+	int32 TablesMin = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 TablesMax = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 ChairsMin = 1;
+	UPROPERTY(EditDefaultsOnly)
+	int32 ChairsMax = 4;
+
+	UFUNCTION(BlueprintCallable)
+	virtual AActor* TakeFurniture();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void MoveFurniture(AActor* Furniture, FVector Location);
 };
